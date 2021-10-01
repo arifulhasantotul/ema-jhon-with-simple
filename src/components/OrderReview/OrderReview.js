e156e25f2e6bbeb7c27c4e0295b645c46eb5426a
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-10-01 10:50:34 +0600

    last

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProduct";
+import { deleteFromDb } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const OrderReview = () => {
-    return (
-        <div>
-            <h2>This is Order Review</h2>
-        </div>
-    );
+   const [products] = useProducts();
+   const [cart, setCart] = useCart(products);
+
+   const removeItem = (key) => {
+      const newCart = cart.filter((product) => product.key !== key);
+      setCart(newCart);
+      deleteFromDb(key);
+   };
+
+   return (
+      <div className="shop-container">
+         <div className="product-container">
+            {cart.map((product) => (
+               <ReviewItem
+                  key={product.key}
+                  product={product}
+                  handleRemove={removeItem}
+               ></ReviewItem>
+            ))}
+         </div>
+         <div className="cart-container">
+            <Cart cart={cart}></Cart>
+         </div>
+      </div>
+   );
 };
 
 export default OrderReview;
